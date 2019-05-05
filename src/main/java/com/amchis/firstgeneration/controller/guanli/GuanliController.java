6a5778be890419eb8d72bda89771e45ac603239b
wenhao <921790476@qq.com>
2019-05-05 17:25:21 +0800

    提交代码

--- src/main/java/com/amchis/firstgeneration/controller/guanli/GuanliController.java
+++ src/main/java/com/amchis/firstgeneration/controller/guanli/GuanliController.java
@@ -1,5 +1,6 @@
 package com.amchis.firstgeneration.controller.guanli;
 
+import com.alibaba.druid.util.StringUtils;
 import com.amchis.firstgeneration.bean.guanli.Register.RegisterBean;
 import com.amchis.firstgeneration.bean.guanli.loginbean.LoginBean;
 import com.amchis.firstgeneration.bean.guanli.shangchuanshijians.ShebeiShijianMoxing;
@@ -8,16 +9,28 @@ import com.amchis.firstgeneration.bean.guanli.user.User;
 import com.amchis.firstgeneration.common.BaseApiService;
 import com.amchis.firstgeneration.common.ReponseVo;
 import com.amchis.firstgeneration.service.guanli.*;
+import com.amchis.firstgeneration.utils.UUIDUtils;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/guanli")
@@ -121,6 +134,17 @@ public class GuanliController extends BaseApiService {
     }
 
     /**
+     * 查询基因检测
+     *
+     * @return
+     */
+    @PostMapping("/daorujiyinjiance")
+    public ReponseVo exportGeneticTestingFile(HttpServletRequest request) {
+        String jueseBianma = (String) request.getAttribute("JueseBianma");
+        return jiYinJianCe20sService.selectJiYinJianCe(jueseBianma);
+    }
+
+    /**
      * 获取app版本
      *
      * @return
@@ -138,6 +162,107 @@ public class GuanliController extends BaseApiService {
     @PostMapping("/shebeishiyongshijian")
     public ReponseVo getDeviceTime(@RequestBody ShebeiShijianMoxingCanshu ShebeiShijianMoxingCanshu) {
         return quShiService.selectByXuliehao(ShebeiShijianMoxingCanshu.getShebeiShijianMoxing().getShoujihao());
+    }
+
+    @ResponseBody
+    @RequestMapping("/readexcel")
+    public ReponseVo readExcel(@RequestParam("file") CommonsMultipartFile file, String fileName) throws Exception {
+        InputStream inputStream = file.getInputStream();
+        String name = file.getOriginalFilename();
+        Workbook workbook = null;
+        String postfix = name.substring(name.lastIndexOf("."), name.length());
+        try {
+            if (".xls".equals(postfix)) {
+                // 针对 2003 Excel 文件
+                workbook = new HSSFWorkbook(new POIFSFileSystem(inputStream));
+            } else {
+                // 针对2007 Excel 文件
+                workbook = new XSSFWorkbook(inputStream);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (int s = 0; s < 1; s++) {
+            Sheet sheet = workbook.getSheetAt(s);
+            int rowCount = sheet.getPhysicalNumberOfRows(); // 获取总行数
+            // 遍历每一行
+            String[] id = new String[1];
+            String templateType = null;//模板类型（1 商务部分，2 报价部分 ，3 技术部分）
+            for (int r = 0; r < rowCount; r++) {
+                Row row = sheet.getRow(r);
+                if (row != null) {
+                    int cellCount = row.getPhysicalNumberOfCells(); // 获取总列数
+                    if (cellCount == 8) {
+                        Cell cell0 = row.getCell(0);//获取审查内容
+                        String v = cell0.getStringCellValue();
+                        Cell cell1 = row.getCell(1);//获取审查标准
+                        Cell cell2 = row.getCell(2);//获取审核
+                        Cell cell3 = row.getCell(3);//获取通知整改记录
+                        if (!StringUtils.isEmpty(cell3.getStringCellValue())) {
+                        }
+                        Cell cell4 = row.getCell(4);//获取备注
+                        if (!StringUtils.isEmpty(cell4.getStringCellValue())) {
+                        }
+                        //result = true;
+                    } else {
+                        //result = false;
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
+
+    @ResponseBody
+    @RequestMapping("/readexcelttttt")
+    public ReponseVo readExcelTTTTTT(@RequestParam("file") CommonsMultipartFile file) throws Exception {
+        InputStream inputStream = file.getInputStream();
+        String name = file.getOriginalFilename();
+        Workbook workbook = null;
+        String postfix = name.substring(name.lastIndexOf("."), name.length());
+        try {
+            if (".xls".equals(postfix)) {
+                // 针对 2003 Excel 文件
+                workbook = new HSSFWorkbook(new POIFSFileSystem(inputStream));
+            } else {
+                // 针对2007 Excel 文件
+                workbook = new XSSFWorkbook(inputStream);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (int s = 0; s < 1; s++) {
+            Sheet sheet = workbook.getSheetAt(s);
+            int rowCount = sheet.getPhysicalNumberOfRows(); // 获取总行数
+            // 遍历每一行
+            String[] id = new String[1];
+            String templateType = null;//模板类型（1 商务部分，2 报价部分 ，3 技术部分）
+            for (int r = 0; r < rowCount; r++) {
+                Row row = sheet.getRow(r);
+                if (row != null) {
+                    int cellCount = row.getPhysicalNumberOfCells(); // 获取总列数
+                    if (cellCount == 6) {
+                        Cell cell0 = row.getCell(0);//获取审查内容
+                        String v = cell0.getStringCellValue();
+                        Cell cell1 = row.getCell(1);//获取审查标准
+                        Cell cell2 = row.getCell(2);//获取审核
+                        Cell cell3 = row.getCell(3);//获取通知整改记录
+                        if (!StringUtils.isEmpty(cell3.getStringCellValue())) {
+                        }
+                        Cell cell4 = row.getCell(4);//获取备注
+                        if (!StringUtils.isEmpty(cell4.getStringCellValue())) {
+                        }
+                        //result = true;
+                    } else {
+                        //result = false;
+                    }
+                }
+            }
+        }
+
+        return null;
     }
 
 }
