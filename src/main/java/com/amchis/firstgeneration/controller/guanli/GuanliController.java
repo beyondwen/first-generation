7c7303c76d37bc96f3a7c175eaa1534c000dccc0
wenhao <921790476@qq.com>
2019-05-06 14:39:33 +0800

    提交代码

--- src/main/java/com/amchis/firstgeneration/controller/guanli/GuanliController.java
+++ src/main/java/com/amchis/firstgeneration/controller/guanli/GuanliController.java
@@ -2,13 +2,19 @@ package com.amchis.firstgeneration.controller.guanli;
 
 import com.alibaba.druid.util.StringUtils;
 import com.amchis.firstgeneration.bean.guanli.Register.RegisterBean;
+import com.amchis.firstgeneration.bean.guanli.jiyinjiance20s.JiYinJianCe20s;
 import com.amchis.firstgeneration.bean.guanli.loginbean.LoginBean;
 import com.amchis.firstgeneration.bean.guanli.shangchuanshijians.ShebeiShijianMoxing;
 import com.amchis.firstgeneration.bean.guanli.shangchuanshijians.ShebeiShijianMoxingCanshu;
 import com.amchis.firstgeneration.bean.guanli.user.User;
+import com.amchis.firstgeneration.bean.huoban.yonghu2s.Yonghu2s;
 import com.amchis.firstgeneration.common.BaseApiService;
+import com.amchis.firstgeneration.common.ErrorVo;
 import com.amchis.firstgeneration.common.ReponseVo;
+import com.amchis.firstgeneration.mapper.huoban.yonghu2s.Yonghu2sMapper;
 import com.amchis.firstgeneration.service.guanli.*;
+import com.amchis.firstgeneration.utils.DateUtils;
+import com.amchis.firstgeneration.utils.GetTime;
 import com.amchis.firstgeneration.utils.UUIDUtils;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
@@ -28,10 +34,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/guanli")
@@ -57,6 +60,9 @@ public class GuanliController extends BaseApiService {
 
     @Autowired
     private QuShiService quShiService;
+
+    @Autowired
+    private Yonghu2sMapper yonghu2sMapper;
 
     /**
      * 登录接口
@@ -165,12 +171,17 @@ public class GuanliController extends BaseApiService {
         return quShiService.selectByXuliehao(ShebeiShijianMoxingCanshu.getShebeiShijianMoxing().getShoujihao());
     }
 
-    @ResponseBody
-    @RequestMapping("/readexcel")
-    public ReponseVo readExcel(@RequestParam("file") CommonsMultipartFile file, String fileName) throws Exception {
+    @PostMapping("/readexcel")
+    public ReponseVo readExcel(HttpServletRequest request, @RequestParam("file") MultipartFile file) throws Exception {
+        //String biaoji = (String) request.getAttribute("Biaoji");
+        //String zhanghao = (String) request.getAttribute("Shoujihao");
         InputStream inputStream = file.getInputStream();
         String name = file.getOriginalFilename();
         Workbook workbook = null;
+        ErrorVo errorVo = new ErrorVo();
+        errorVo.setBianma("LURU_SHIBAI");
+        errorVo.setMiaoshu("录入失败");
+        ReponseVo reponseVo = new ReponseVo("0", errorVo);
         String postfix = name.substring(name.lastIndexOf("."), name.length());
         try {
             if (".xls".equals(postfix)) {
@@ -186,82 +197,73 @@ public class GuanliController extends BaseApiService {
         for (int s = 0; s < 1; s++) {
             Sheet sheet = workbook.getSheetAt(s);
             int rowCount = sheet.getPhysicalNumberOfRows(); // 获取总行数
-            // 遍历每一行
-            String[] id = new String[1];
-            String templateType = null;//模板类型（1 商务部分，2 报价部分 ，3 技术部分）
+            List<JiYinJianCe20s> list = new LinkedList<>();
             for (int r = 0; r < rowCount; r++) {
-                Row row = sheet.getRow(r);
+                Row row = sheet.getRow(r + 1);
                 if (row != null) {
                     int cellCount = row.getPhysicalNumberOfCells(); // 获取总列数
                     if (cellCount == 8) {
-                        Cell cell0 = row.getCell(0);//获取审查内容
-                        String v = cell0.getStringCellValue();
-                        Cell cell1 = row.getCell(1);//获取审查标准
-                        Cell cell2 = row.getCell(2);//获取审核
-                        Cell cell3 = row.getCell(3);//获取通知整改记录
-                        if (!StringUtils.isEmpty(cell3.getStringCellValue())) {
+                        row.getCell(0).setCellType(Cell.CELL_TYPE_STRING);
+                        String yonghuming = row.getCell(0).getStringCellValue();//获取用户名
+                        row.getCell(1).setCellType(Cell.CELL_TYPE_STRING);
+                        String shoujihao = row.getCell(1).getStringCellValue();//获取手机号
+                        row.getCell(2).setCellType(Cell.CELL_TYPE_STRING);
+                        String cdka1 = row.getCell(2).getStringCellValue();//获取cdka1
+                        row.getCell(3).setCellType(Cell.CELL_TYPE_STRING);
+                        String cdka2 = row.getCell(3).getStringCellValue();//获取cdka2
+                        row.getCell(4).setCellType(Cell.CELL_TYPE_STRING);
+                        String cdkn2a = row.getCell(4).getStringCellValue();//获取cdkn2a
+                        row.getCell(5).setCellType(Cell.CELL_TYPE_STRING);
+                        String hhex = row.getCell(5).getStringCellValue();//获取hhex
+                        row.getCell(6).setCellType(Cell.CELL_TYPE_STRING);
+                        String slc30a8 = row.getCell(6).getStringCellValue();//获取slc30a8
+                        row.getCell(7).setCellType(Cell.CELL_TYPE_STRING);
+                        String kcnq1 = row.getCell(7).getStringCellValue();//获取kcnq1
+                        JiYinJianCe20s jiYinJianCe20s = new JiYinJianCe20s();
+                        Yonghu2s yonghu2s = yonghu2sMapper.chaxunyonghuByshouji(shoujihao);
+                        if (yonghu2s != null) {
+                            jiYinJianCe20s.setWaijian(GetTime.getGuid());
+                            jiYinJianCe20s.setRiqi(DateUtils.getDay());
+                            jiYinJianCe20s.setYonghuwaijian(yonghu2s.getWaijian());
+                            jiYinJianCe20s.setCdkal11(cdka1);
+                            jiYinJianCe20s.setCdkal12(cdka2);
+                            jiYinJianCe20s.setCdkn2aB(cdkn2a);
+                            jiYinJianCe20s.setHhex(hhex);
+                            jiYinJianCe20s.setSlc30a8(slc30a8);
+                            jiYinJianCe20s.setKcnq1(kcnq1);
+                            jiYinJianCe20s.setLuruduan("DAORU");
+                            jiYinJianCe20s.setYonghumingcheng(yonghuming);
+                        } else {
+                            jiYinJianCe20s.setWaijian(GetTime.getGuid());
+                            jiYinJianCe20s.setRiqi(DateUtils.getDay());
+                            jiYinJianCe20s.setYonghuwaijian(shoujihao);
+                            jiYinJianCe20s.setCdkal11(cdka1);
+                            jiYinJianCe20s.setCdkal12(cdka2);
+                            jiYinJianCe20s.setCdkn2aB(cdkn2a);
+                            jiYinJianCe20s.setHhex(hhex);
+                            jiYinJianCe20s.setSlc30a8(slc30a8);
+                            jiYinJianCe20s.setKcnq1(kcnq1);
+                            jiYinJianCe20s.setLuruduan("DAORU");
+                            jiYinJianCe20s.setYonghumingcheng(yonghuming);
                         }
-                        Cell cell4 = row.getCell(4);//获取备注
-                        if (!StringUtils.isEmpty(cell4.getStringCellValue())) {
+                        int i = jiYinJianCe20sService.insertSelective(jiYinJianCe20s);
+                        if (!toDaoResult(i)) {
+                            errorVo.setBianma("LURU_SHIBAI");
+                            errorVo.setMiaoshu("录入失败");
+                            reponseVo = new ReponseVo("0", errorVo);
+                        } else {
+                            list.add(jiYinJianCe20s);
+                            reponseVo = new ReponseVo("1", list);
                         }
-                        //result = true;
                     } else {
-                        //result = false;
+                        errorVo.setBianma("BAOCUN_CUOWU");
+                        errorVo.setMiaoshu("文件列数为8列，请检查");
+                        reponseVo = new ReponseVo("0", errorVo);
                     }
                 }
             }
         }
-
-        return null;
-    }
-
-
-    @ResponseBody
-    @RequestMapping("/readexcelttttt")
-    public ReponseVo readExcelTTTTTT(@RequestParam("file") MultipartFile file) throws Exception {
-        InputStream inputStream = file.getInputStream();
-        String name = file.getOriginalFilename();
-        Workbook workbook = null;
-        String postfix = name.substring(name.lastIndexOf("."), name.length());
-        try {
-            if (".xls".equals(postfix)) {
-                // 针对 2003 Excel 文件
-                workbook = new HSSFWorkbook(new POIFSFileSystem(inputStream));
-            } else {
-                // 针对2007 Excel 文件
-                workbook = new XSSFWorkbook(inputStream);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        for (int s = 0; s < 1; s++) {
-            Sheet sheet = workbook.getSheetAt(s);
-            int rowCount = sheet.getPhysicalNumberOfRows(); // 获取总行数
-            // 遍历每一行
-            for (int r = 0; r < rowCount; r++) {
-                Row row = sheet.getRow(r);
-                if (row != null) {
-                    int cellCount = row.getPhysicalNumberOfCells(); // 获取总列数
-                    if (cellCount == 6) {
-                        Cell cell0 = row.getCell(0);//获取审查内容
-                        String v = cell0.getStringCellValue();
-                        Cell cell1 = row.getCell(1);//获取审查标准
-                        Cell cell2 = row.getCell(2);//获取审核
-                        Cell cell3 = row.getCell(3);//获取通知整改记录
-                        if (!StringUtils.isEmpty(cell3.getStringCellValue())) {
-                        }
-                        Cell cell4 = row.getCell(4);//获取备注
-                        if (!StringUtils.isEmpty(cell4.getStringCellValue())) {
-                        }
-                        //result = true;
-                    } else {
-                        //result = false;
-                    }
-                }
-            }
-        }
-
-        return null;
+        return reponseVo;
     }
 
 }
